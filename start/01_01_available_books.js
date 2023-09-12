2d79d747f11e8df3982db621a80c26e5fa3af8dc
alexsime07 <143498552+alexsime07@users.noreply.github.com>
2023-09-12 13:56:06 +0000

    Available books class with the functions of checking availability and sell & restock functions.

--- start/01_01_available_books.js
+++ start/01_01_available_books.js
@@ -1,1 +1,40 @@
 // Write your code here
+class Book { 
+  constructor (title, author, ISBN, numCopies) {
+    this.title = title;
+    this.author = author;
+    this.ISBN = ISBN;
+    this.numCopies = numCopies;
+  }
+
+  get availability () {
+    return this.getAvailability()
+  }
+
+  getAvailability () {
+    if (this.numCopies === 0) {
+      return 'Out of stock'
+    } else if (this.numCopies < 10) {
+      return 'Low stock'
+    } else {
+      return 'In stock'
+    }
+  }
+
+  sell(numCopiesSold = 1) {
+    this.numCopies -= numCopiesSold
+  }
+
+  restock(numCopiesStocked = 5) {
+    this.numCopies += numCopiesStocked
+  }
+
+  }
+  const RulesForLife = new Book ('Rules for Life', 'Jordan Peterson', 12321, 200);
+  console.log(RulesForLife.availability)
+  RulesForLife.sell(192)
+  console.log(RulesForLife.availability)
+  RulesForLife.restock(100)
+    console.log(RulesForLife.availability)
+  RulesForLife.sell(108)
+    console.log(RulesForLife.availability)

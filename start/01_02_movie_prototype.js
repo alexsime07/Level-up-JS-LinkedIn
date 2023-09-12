a2b5a9ef1527d4d55fc69fad40258a226e0dbb49
alexsime07 <143498552+alexsime07@users.noreply.github.com>
2023-09-12 19:49:52 +0000

    Made the movie prototype using class.

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,17 @@
 // Write your code here
+class Movie {
+  constructor (title, director, genre, releaseYear, rating) {
+    this.title = title,
+    this.director = director,
+    this.genre = genre,
+    this.releaseYear = releaseYear,
+    this.rating = rating;
+  }
+
+  getOverview () {
+      return `${this.title}, a ${this.genre} film directed by ${this.director} was
+       released in ${this.releaseYear}. It received a rating of ${this.rating}.`;
+    }
+}
+const Matrix = new Movie ('Matrix', 'Michael Bay', 'Action', 2001, 91)
+console.log(Matrix.getOverview());
